ccad71ab2816b5a136b73621a5855d206b8b24c4
BeeMeme Official <157950866+BeeMeme@users.noreply.github.com>
2024-06-12 16:50:58 +0530

    Delete src directory

--- src/themes/typography.js
+++
@@ -1,9 +0,0 @@
-const themeTypography = () => ({
-    fontFamily: `-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',sans-serif`,
-    modalFormLabel: {
-        fontSize: '16px'
-    }
-});
-
-export default themeTypography;
